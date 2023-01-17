9984f9668549cea9c9e5d5417d4bb0db0be53267
PrikolMen <olegis019@gmail.com>
2023-01-17 15:40:28 +0400

    example update

--- lua/phats/config.lua
+++ lua/phats/config.lua
@@ -4,6 +4,7 @@ local phats = phats
 
 local hat = phats.Register( 'prikolmen', 'models/player/items/humans/top_hat.mdl' )
 hat:AddModel( 'models/player/Group03/female_04.mdl' )
+hat:AddModel( 'models/player/Group03/female_02.mdl' )
 hat:AddSteamID( 'STEAM_0:1:70096775' )
 hat:SetPos( Vector( -3, 0, -3 ) )
 
