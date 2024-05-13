b62d2107a42a5a269875f0a4f808d0d23eed70e9
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2024-05-13 12:12:56 +0400

    player-hats v2

--- lua/autorun/sh_phats.lua
+++
@@ -1,9 +0,0 @@
-if (SERVER) then
-    AddCSLuaFile( 'phats/config.lua' )
-    AddCSLuaFile( 'phats/cl_lib.lua' )
-end
-
-if (CLIENT) then
-    include( 'phats/cl_lib.lua' )
-    include( 'phats/config.lua' )
-end
